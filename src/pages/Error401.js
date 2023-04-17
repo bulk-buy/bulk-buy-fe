0928e07d541e03c9f2de95f6004820289b499f97
terenceong95 <terenceong95@gmail.com>
2023-04-17 14:10:26 +0800

    Created error 401 and 404 pages

---
+++ src/pages/Error401.js
@@ -0,0 +1,8 @@
+import { Paper } from "@mui/material";
+import React from "react";
+
+function Error401() {
+  return <Paper elevation={0}>Error 401 page</Paper>;
+}
+
+export default Error401;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,20 @@
-const { default: Home } = require("pages/home");
+import Error401 from "pages/Error401";
+import Error404 from "pages/Error404";
+
+const { default: Home } = require("pages/Home");
 const { createBrowserRouter } = require("react-router-dom");
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/401",
+    element: <Error401 />,
+  },
+  {
+    path: "/404",
+    element: <Error404 />,
   },
 ]);
