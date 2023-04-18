6a8671d2f70fb185d9ac90588d415a20951ab113
Terence Ong <terenceong95@gmail.com>
2023-04-18 16:54:37 +0800

    Added aws authenticator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import LeftNavigation from "components/sidebar/LeftNavigation";
 import { RouterProvider } from "react-router-dom";
 import { router } from "routes/routes";
 
 function App() {
   return (
-    <RouterProvider router={router}>
-      <LeftNavigation />
-    </RouterProvider>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <RouterProvider router={router}>
+          <LeftNavigation />
+        </RouterProvider>
+      )}
+    </Authenticator>
   );
 }
 
