b1e62c33d93fb651d5717a528bfd25ded51ce6ab
Terence Ong <terenceong95@gmail.com>
2023-04-19 16:51:46 +0800

    Added logo and theming

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -1,3 +1,14 @@
 const { createTheme } = require("@mui/material");
 
-export const defaultTheme = createTheme({});
+export const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#2e2e2e",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#efe494",
+      contrastText: "#000",
+    },
+  },
+});
