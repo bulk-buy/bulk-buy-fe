de3287368427026038aa2c758b81ff32fc479d33
e0276472 <109505551+e0276472@users.noreply.github.com>
2023-04-28 15:28:29 +0800

    Chore(Client):Added image gif to landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
-import { Paper } from "@mui/material";
-import React from "react";
+import React from 'react';
+import './style.css'; // import the CSS file
+import trolley128 from './images/trolley128.png';
+import labelImage1 from './images/bulk-buy-ad.gif';
 
 function Home() {
-  return <Paper elevation={0}>landing page</Paper>;
+  return (
+    <div className="container">
+      <h1>Welcome to Bulk Buy Purchase</h1>
+      <img src={labelImage1} alt="Bulk Buy" className="mainImage" />
+    </div>
+  );
 }
 
 export default Home;
